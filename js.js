94d5a7b6349cb8ee7431586f7c9376367cb83c45
ASIF RAZA <158028070+itx-asif@users.noreply.github.com>
2025-01-26 22:25:13 +0500

    updated about a bit

--- js.js
+++ js.js
@@ -1,9 +1,14 @@
-function showSection(sectionId) {
+function showSection(evt,sectionId) {
     // Hide all sections
     document.querySelectorAll('.section').forEach(section => {
-        section.classList.add('hidden');
+        section.classList.add('hidden')
+        
     });
 
     // Show the selected section
     document.getElementById(sectionId).classList.remove('hidden');
+    document.querySelectorAll('.tabs').forEach(tab=>{
+        tab.classList.remove("active")
+    })
+    evt.currentTarget.className += " active";
 }
